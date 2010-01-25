7efc60b408c2724c6f80c0adad860baca187bcb0
Patrick Reagan <patrick.reagan@viget.com>
2010-01-25 14:50:33 -0500

    Fixed include to work with ActiveSupport >= 2.3.5

--- lib/graft/xml.rb
+++ lib/graft/xml.rb
@@ -3,7 +3,15 @@ $:.unshift File.dirname(__FILE__) + '/..'
 require 'hpricot'
 require 'builder'
 require 'tzinfo'
-require 'active_support/core_ext/blank'
+
+begin
+  # ActiveSupport < 2.3.5
+  require 'active_support/core_ext/blank'
+rescue NameError
+  # ActiveSupport >= 2.3.5 will raise a NameError exception
+  require 'active_support/core_ext/object/blank'
+end
+
 require 'active_support/time_with_zone'
 require 'active_support/inflector'
 
